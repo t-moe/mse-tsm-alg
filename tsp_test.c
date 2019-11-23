8d60dbd26d3840cadd2284d6e84b57f2d2e556f7
Timo Lang <langtim1@students.zhaw.ch>
2019-11-23 23:38:40 +0100

    Autorun tests

--- tsp_test.c
+++ tsp_test.c
@@ -21,6 +21,7 @@
 #include "tsp_pilot.h"
 #include "tsp_local_greedy.h"
 #include "tsp_local_improv.h"
+#include <string.h>
 
 /* For sorting a table of double by non-decreasing value */
 int larger(const void *a, const void *b)
@@ -47,90 +48,115 @@ int main(void)
   char filename[80];                                     /* Name of data file */
   char trash[1000];             /* Skipping the reading up to 1000 characters */
 
-  printf("Data file name : \n");
+  //printf("Data file name : \n");
   //dummy_int = scanf("%s",filename);
   // data_file = fopen(filename,"r");
-  data_file = fopen("/home/timo/study/tsmalg/Exercises2_CODE/TSP_Instances/bier127.tsp" , "r");
-  //data_file = fopen("/home/timo/study/tsmalg/Exercises2_CODE/TSP_Instances/berlin52.tsp", "r");
-  //data_file = fopen("/home/timo/study/tsmalg/Exercises2_CODE/TSP_Instances/pr2392.tsp", "r");
-  //data_file = fopen("/home/timo/study/tsmalg/Exercises2_CODE/TSP_Instances/pr1002.tsp", "r");
-  // data_file = fopen("/home/timo/study/tsmalg/Exercises2_CODE/TSP_Instances/tsp225.tsp", "r");
-
-  /* Reading the data file. Hoping in right format!! ALMOST NO CHECKS!!       */
-  dummy_int = fscanf(data_file, "%[^\n]\n", trash);
-  dummy_int = fscanf(data_file, "%[^\n]\n", trash);
-  dummy_int = fscanf(data_file, "%[^\n]\n", trash);
-  if (fscanf(data_file, "DIMENSION : %u\n", &n) )
-    printf("%d\n",n);   /* Checking if the number of cities is correctly read */
-  else
-    return EXIT_FAILURE;
-  dummy_int = fscanf(data_file, "%[^\n]\n", trash);
-  dummy_int = fscanf(data_file, "%[^\n]\n", trash);
-
- /* Memory allocation */
-  x = (double*)malloc(n * sizeof(double));
-  y = (double*)malloc(n * sizeof(double));
-  solution = (unsigned*) malloc(n * sizeof(unsigned));
-  d = (double**) malloc(n * sizeof(double*));
-  for (i = 0; i < n; i++)
-  {
-    d[i] =  (double*) malloc(n * sizeof(double));
-      if (!d[i])
-      { printf("Instance too large: Memory cannot be allocated!\n");
-        getchar(); getchar();
-        return EXIT_FAILURE;
-      }
-  }
-
-  /* Reading the cities coordinates */
-  for (i = 0; i < n; i++)
-    if (fscanf(data_file, "%u %lf %lf", &dummy_int, &x[i], &y[i]) != 3)
-      return EXIT_FAILURE;
-  fclose(data_file);
-  /******************************** end reading ******************************/
-
-  /* Compute distance matrix and absolute numerical precision */
-  dmax = 0.0;
-  for (i = 0; i < n; i++)
-    for (j = 0; j < n; j++)
-    {
-      d[i][j] = dist(x[i],y[i], x[j],y[j]);
-      if (d[i][j] > dmax)
-        dmax = d[i][j];
-    }
-  epsilon *= dmax;
 
 
+    char* files [] = {
+            "berlin52",
+            "bier127",
+            "tsp225",
+            "pr1002",
+            "pr2392",
+            "rl5915"
+    };
 
-  //length = tsp_random_samp(n, d, solution);
-    //length = tsp_nearest_neighbor(n, d, solution);
-    //length = tsp_pilot(n, d, solution);
-   // length = tsp_local_greedy(n, d, solution);
+    for(int fn=0; fn < sizeof(files)/sizeof(char*); fn++) {
+        char fname[100] = "../TSP_Instances/";
+        strcpy(fname + strlen(fname), files[fn]);
+        strcpy(fname + strlen(fname), ".tsp");
+        printf("File: %s\n", files[fn]);
 
-    cpu = clock();
-    length = tsp_greedy_insertion(n, d, solution);
-    printf("%d\t%e\t%e (number of cities, time, length) of greedy insertion. \n", n, seconds(cpu), length);
-    draw(n, x, y, solution, "greedy.ps");
 
-    /*cpu = clock();
-    length = tsp_nearest_neighbor(n, d, solution);
-    printf("%d\t%e\t%e (number of cities, time, length) of nearest neighbor. \n", n, seconds(cpu), length);
-    draw(n, x, y, solution, "nneighbor.ps");
+        char fname_picture[100] = "./";
+        strcpy(fname_picture + strlen(fname_picture), files[fn]);
+        strcpy(fname_picture + strlen(fname_picture), "_localimprov.ps");
+        printf("Picture file name: %s\n", fname_picture);
 
-    cpu = clock();
-    length = tsp_local_greedy(n, d, solution);
-    printf("%d\t%e\t%e (number of cities, time, length) of local greedy insertion. \n", n, seconds(cpu), length);
-    draw(n, x, y, solution, "localgreedy.ps");*/
+        data_file = fopen(fname, "r");
 
-    /*cpu = clock();
-    length = tsp_pilot(n, d, solution);
-    printf("%d\t%e\t%e (number of cities, time, length) of pilot. \n", n, seconds(cpu), length);
-    draw(n, x, y, solution, "pilot.ps");*/
 
-    cpu = clock();
-    length = tsp_local_improv(n, d, solution);
-    printf("%d\t%e\t%e (number of cities, time, length) of one local improvments. \n", n, seconds(cpu), length);
-    draw(n, x, y, solution, "localimprov.ps");
+        //data_file = fopen("/home/timo/study/tsmalg/Exercises2_CODE/TSP_Instances/bier127.tsp" , "r");
+        //data_file = fopen("/home/timo/study/tsmalg/Exercises2_CODE/TSP_Instances/berlin52.tsp", "r");
+        //data_file = fopen("/home/timo/study/tsmalg/Exercises2_CODE/TSP_Instances/pr2392.tsp", "r");
+        //data_file = fopen("/home/timo/study/tsmalg/Exercises2_CODE/TSP_Instances/pr1002.tsp", "r");
+        // data_file = fopen("/home/timo/study/tsmalg/Exercises2_CODE/TSP_Instances/tsp225.tsp", "r");
+
+        /* Reading the data file. Hoping in right format!! ALMOST NO CHECKS!!       */
+        dummy_int = fscanf(data_file, "%[^\n]\n", trash);
+        dummy_int = fscanf(data_file, "%[^\n]\n", trash);
+        dummy_int = fscanf(data_file, "%[^\n]\n", trash);
+        if (fscanf(data_file, "DIMENSION : %u\n", &n))
+            printf("%d\n", n);   /* Checking if the number of cities is correctly read */
+        else
+            return EXIT_FAILURE;
+        dummy_int = fscanf(data_file, "%[^\n]\n", trash);
+        dummy_int = fscanf(data_file, "%[^\n]\n", trash);
+
+        /* Memory allocation */
+        x = (double *) malloc(n * sizeof(double));
+        y = (double *) malloc(n * sizeof(double));
+        solution = (unsigned *) malloc(n * sizeof(unsigned));
+        d = (double **) malloc(n * sizeof(double *));
+        for (i = 0; i < n; i++) {
+            d[i] = (double *) malloc(n * sizeof(double));
+            if (!d[i]) {
+                printf("Instance too large: Memory cannot be allocated!\n");
+                getchar();
+                getchar();
+                return EXIT_FAILURE;
+            }
+        }
+
+        /* Reading the cities coordinates */
+        for (i = 0; i < n; i++)
+            if (fscanf(data_file, "%u %lf %lf", &dummy_int, &x[i], &y[i]) != 3)
+                return EXIT_FAILURE;
+        fclose(data_file);
+        /******************************** end reading ******************************/
+
+        /* Compute distance matrix and absolute numerical precision */
+        dmax = 0.0;
+        for (i = 0; i < n; i++)
+            for (j = 0; j < n; j++) {
+                d[i][j] = dist(x[i], y[i], x[j], y[j]);
+                if (d[i][j] > dmax)
+                    dmax = d[i][j];
+            }
+        epsilon *= dmax;
+
+
+
+        //length = tsp_random_samp(n, d, solution);
+        //length = tsp_nearest_neighbor(n, d, solution);
+        //length = tsp_pilot(n, d, solution);
+        // length = tsp_local_greedy(n, d, solution);
+
+        cpu = clock();
+        length = tsp_greedy_insertion(n, d, solution);
+        printf("%d\t%e\t%e (number of cities, time, length) of greedy insertion. \n", n, seconds(cpu), length);
+        draw(n, x, y, solution, "greedy.ps");
+
+        /*cpu = clock();
+        length = tsp_nearest_neighbor(n, d, solution);
+        printf("%d\t%e\t%e (number of cities, time, length) of nearest neighbor. \n", n, seconds(cpu), length);
+        draw(n, x, y, solution, "nneighbor.ps");
+
+        cpu = clock();
+        length = tsp_local_greedy(n, d, solution);
+        printf("%d\t%e\t%e (number of cities, time, length) of local greedy insertion. \n", n, seconds(cpu), length);
+        draw(n, x, y, solution, "localgreedy.ps");*/
+
+        /*cpu = clock();
+        length = tsp_pilot(n, d, solution);
+        printf("%d\t%e\t%e (number of cities, time, length) of pilot. \n", n, seconds(cpu), length);
+        draw(n, x, y, solution, "pilot.ps");*/
+
+        cpu = clock();
+        length = tsp_local_improv(n, d, solution);
+        printf("%d\t%e\t%e (number of cities, time, length) of one local improvments. \n", n, seconds(cpu), length);
+        draw(n, x, y, solution, fname_picture);
 
 
 /*
@@ -168,6 +194,7 @@ int main(void)
   draw(n, x, y, solution, "grasp.ps");
 */
 
+    }
   printf("Press return to end ...");
   getchar(); getchar();
   return EXIT_SUCCESS;
